2ef4deb0a9da8c6b01b598b8516e05006802acef
leobm <neokolor@gmx.de>
2022-09-23 00:39:54 +0200

    dynamic import of node crypto library

--- src/Simple/ULID.js
+++ src/Simple/ULID.js
@@ -1,14 +1,13 @@
 'use strict';
 
-import * as nodeCrypto from "crypto";
-
-export function prng() {
+export async function prng() {
   var webCrypto = typeof window !== "undefined" && (window.crypto || window.msCrypto);
   if (webCrypto) {
     var buf = new Uint8Array(1);
     webCrypto.getRandomValues(buf);
     return buf[0] / 0xff;
   } else {
-    return nodeCrypto.randomBytes(1).readUInt8() / 0xff;
+    const { randomBytes } = await import('crypto');
+    return randomBytes(1).readUInt8() / 0xff;
   }
 }
